75c20a480995b20754eae0aa38e895ea24a45d70
jogaleumesh <jogaleumesh@gmail.com>
2024-11-12 17:18:51 +0530

    Episode-05 | Let's Get Hooked

---
+++ src/components/Body.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+import RestaurantCard from "./RestaurantCard";
+import restList from "../utils/mockData";
+
+const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(restList);
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filterList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestaurants(filterList);
+          }}
+        >
+          Top Rated restaurant
+        </button>
+      </div>
+
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} restData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
--- src/utils/mockData.js
+++ src/utils/mockData.js
@@ -1,53 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-const Header = () => {
-  return (
-    <div className="header">
-      <div className="logo-container">
-        <img
-          className="logo"
-          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?size=2"
-          alt="logo"
-        />
-      </div>
-      <div className="nav-items">
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-const RestaurantCard = (props) => {
-  const { restData } = props;
-  const { name, cloudinaryImageId, cuisines, avgRating, costForTwo, sla } =
-    restData?.info;
-
-  return (
-    <div className="res-card">
-      <img
-        className="res-logo"
-        alt="res-logo"
-        src={
-          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
-          cloudinaryImageId
-        }
-      />
-      <h3>{name}</h3>
-      <h4>{cuisines.join(", ")}</h4>
-      <h4>{avgRating} stars</h4>
-      <h4>{costForTwo}</h4>
-      <h4>{sla.deliveryTime} minutes</h4>
-    </div>
-  );
-};
-
-const restList = [
+const resList = [
   {
     info: {
       id: "234875",
@@ -904,27 +855,4 @@ const restList = [
   },
 ];
 
-const Body = () => {
-  return (
-    <div className="body">
-      <div className="search">Search</div>
-      <div className="res-container">
-        {restList.map((restaurant) => (
-          <RestaurantCard key={restaurant.info.id} restData={restaurant} />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-const App = () => {
-  return (
-    <div className="app">
-      <Header />
-      <Body />
-    </div>
-  );
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+export default resList;
